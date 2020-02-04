ebbe0738f8c41cb481043323fd12b2f348f7e74c
Darren <dartrenbbx@gmail.com>
2020-02-03 21:48:26 -0800

    can parse through links

--- hw2/spacetime-crawler4py-master/scraper.py
+++ hw2/spacetime-crawler4py-master/scraper.py
@@ -2,50 +2,79 @@ import re
 from urllib.parse import urlparse
 from urllib.parse import urldefrag
 from lxml import html
+import os
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    validLinks = set([link for link in links if is_valid(link)])
+    print(validLinks)
+    finalLinks = Visited(validLinks)
+    print(finalLinks)
+    return list(finalLinks)
 
 def extract_next_links(url, resp):
     # Implementation requred.
+    # add robots.txt
     # check if valid page
     # defrag links
 
+    # check if url responds
     if resp.status == 200:
         print("SUCCESS")
     elif resp.status == 404:
         print("FAIL")
         return list()
 
-    print(url)
     defrag = urldefrag(url)[0]
+
+    print(url)
     print(defrag)
-
-    extracted_links = list()
-
-    visited = open("visitedURLs.txt", "w")
-    visited.write(defrag)
+    visited = open('visitedURLs.txt', 'a+')
+    visited.write(defrag + "\n")
     visited.close()
 
-    print(is_valid(defrag))
+    #if Visited(url):
+    #   return list()
 
-    return list()
+    UniqueURLs(defrag)
+
+    # extract links on the url page
+    extracted_links = set()
+    try:
+        content = resp.raw_response.content
+        dom = html.fromstring(content)
+        for link in dom.xpath('//a/@href'):
+            if link[0] == '#' or len(link) == 1:
+                print("cont")
+                continue
+            elif link[0] == '/' and link[1] != '/':
+                link = urlparse(url).netloc + link
+            print(link)
+            #defragged = urldefrag(link)[0]
+            extracted_links.add(link)
+    except:
+        print("Page content unavailable")
+        print(url)
+
+    #print(is_valid(defrag))
+    return extracted_links
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
         isInDomain = False
-        domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu", "today.uci.edu/department/information_computer_sciences"]
+        domains = ['ics.uci.edu', 'cs.uci.edu', 'informatics.uci.edu', 'stat.uci.edu', 'today.uci.edu/department/information_computer_sciences']
+
         if parsed.scheme not in set(["http", "https"]):
             return False
+
         for domain in domains:
-            if domain in parsed.netloc:
+            if domain in parsed.netloc or ('today.uci.edu' in parsed.netloc and '/department/information_computer_sciences' in parsed.path):
                 isInDomain = True
-                print(domain)
                 break
         if not isInDomain:
             return isInDomain
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -57,5 +86,66 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise
+
+
+def Visited(urlList):
+    urlSet = []
+    #finalList = []
+    # visited = open("visitedURLs.txt", "a+")
+    #lines = visited.readlines()
+    with open('visitedURLs.txt', 'a+') as visited:
+        for line in visited:
+            urlSet.append(line)
+        urlSet = set(urlSet)
+        urlList = set(urlList)
+        finalSet = urlList.difference(urlSet)
+        for url in finalSet:
+            visited.write(url + "\n")
+
+    '''
+    for url in urlList:
+        if url in urlSet:
+            continue
+        else:
+            finalList.append(url)
+    '''
+    #visited.close()
+    return finalSet
+
+
+def UniqueURLs(defrag):
+    # check if url is unique
+    urlExists = False
+    urlSet = set()
+    with open('uniqueURLs.txt', 'a+') as unique:
+        for line in unique:
+            urlSet.add(line)
+        if defrag not in urlSet:
+            print("writing url")
+            unique.write(defrag + "\n")
+        '''
+            if line == defrag:
+                urlExists = True
+                break
+        if not urlExists:
+            print("writing url")
+            unique.write(defrag + "\n")
+        '''
+
+    '''
+    unique = open("uniqueURLs.txt", "a+")
+    lines = unique.readlines()
+    urlExists = False
+    for line in lines:
+        if line == defrag:
+            urlExists = True
+            break
+    if not urlExists:
+        print("writing url")
+        unique.write(defrag + "\n")
+        print(defrag)
+    unique.close()
+    '''
+
